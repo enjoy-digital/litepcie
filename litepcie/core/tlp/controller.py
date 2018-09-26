288c5f9508e5aa14bd500280727c5167c86000af
Florent Kermarrec <florent@enjoy-digital.fr>
2018-09-26 16:27:10 +0200

    core/tlp/reordering: refactor/simplify

--- litepcie/core/tlp/controller.py
+++ litepcie/core/tlp/controller.py
@@ -5,8 +5,6 @@ from litepcie.core.common import *
 from litepcie.core.tlp.common import *
 from litepcie.core.tlp.reordering import LitePCIeTLPReordering
 
-from migen.genlib.fifo import SyncFIFO
-
 
 class LitePCIeTLPController(Module):
     def __init__(self, data_width, max_pending_requests, with_reordering=False):
@@ -18,34 +16,29 @@ class LitePCIeTLPController(Module):
         req_sink, req_source = self.master_in.sink, self.master_out.sink
         cmp_sink, cmp_source = self.master_out.source, self.master_in.source
 
-        tag_fifo = SyncFIFO(log2_int(max_pending_requests), max_pending_requests)
-        self.submodules += tag_fifo
+        tags = SyncFIFO([("data", log2_int(max_pending_requests))], max_pending_requests)
+        self.submodules += tags
 
         info_mem = Memory(16, max_pending_requests)
         info_mem_wr_port = info_mem.get_port(write_capable=True)
         info_mem_rd_port = info_mem.get_port(async_read=False)
         self.specials += info_mem, info_mem_wr_port, info_mem_rd_port
 
-        req_tag = Signal(log2_int(max_pending_requests))
-        self.sync += \
-            If(tag_fifo.re,
-                req_tag.eq(tag_fifo.dout)
-            )
-
         # Requests mgt
         self.submodules.req_fsm = req_fsm = FSM(reset_state="IDLE")
         req_fsm.act("IDLE",
-            If(req_sink.valid & req_sink.first & ~req_sink.we & tag_fifo.readable,
-                tag_fifo.re.eq(1),
-                NextState("SEND_READ")
-            ).Elif(req_sink.valid & req_sink.first & req_sink.we,
-                NextState("SEND_WRITE")
+            If(req_sink.valid & req_sink.first,
+                If(req_sink.we,
+                   NextState("SEND_WRITE")
+                ).Elif(tags.source.valid,
+                   NextState("SEND_READ")
+                )
             )
         )
         self.comb += req_sink.connect(req_source, omit=set(["valid", "ready"]))
         req_fsm.act("SEND_READ",
             req_source.valid.eq(req_sink.valid),
-            req_source.tag.eq(req_tag),
+            req_source.tag.eq(tags.source.data),
             If(req_source.valid & req_source.last & req_source.ready,
                 NextState("UPDATE_INFO_MEM")
             ).Else(
@@ -61,40 +54,36 @@ class LitePCIeTLPController(Module):
             )
         )
         self.comb += [
-            info_mem_wr_port.adr.eq(req_tag),
+            info_mem_wr_port.adr.eq(tags.source.data),
             info_mem_wr_port.dat_w[:8].eq(req_sink.channel),
             info_mem_wr_port.dat_w[8:].eq(req_sink.user_id)
         ]
         req_fsm.act("UPDATE_INFO_MEM",
             info_mem_wr_port.we.eq(1),
             req_sink.ready.eq(1),
+            tags.source.ready.eq(1),
             NextState("IDLE")
         )
 
-
         # Completions mgt
         if with_reordering:
-            self.submodules.reordering = LitePCIeTLPReordering(data_width, max_pending_requests)
+            reordering = LitePCIeTLPReordering(data_width, max_pending_requests)
+            self.submodules += reordering
             self.comb += [
-                self.reordering.req_we.eq(info_mem_wr_port.we),
-                self.reordering.req_tag.eq(info_mem_wr_port.adr),
-                self.reordering.source.connect(cmp_source)
+                reordering.tag.valid.eq(info_mem_wr_port.we),
+                reordering.tag.data.eq(info_mem_wr_port.adr),
+                reordering.source.connect(cmp_source)
             ]
-            cmp_source = self.reordering.sink
+            cmp_source = reordering.sink
+
+        tag_init = Signal(log2_int(max_pending_requests))
 
         self.submodules.cmp_fsm = cmp_fsm = FSM(reset_state="INIT")
-        tag_cnt = Signal(log2_int(max_pending_requests))
-        inc_tag_cnt = Signal()
-        self.sync += \
-            If(inc_tag_cnt,
-                tag_cnt.eq(tag_cnt + 1)
-            )
-
         cmp_fsm.act("INIT",
-            inc_tag_cnt.eq(1),
-            tag_fifo.we.eq(1),
-            tag_fifo.din.eq(tag_cnt),
-            If(tag_cnt == (max_pending_requests-1),
+            tags.sink.valid.eq(1),
+            tags.sink.data.eq(tag_init),
+            NextValue(tag_init, tag_init + 1),
+            If(tag_init == (max_pending_requests-1),
                 NextState("IDLE")
             )
         )
@@ -112,22 +101,13 @@ class LitePCIeTLPController(Module):
             )
         )
         cmp_fsm.act("COPY",
-            If(cmp_sink.valid & cmp_sink.last & cmp_sink.end,
-                NextState("UPDATE_TAG_FIFO"),
-            ).Else(
-                cmp_source.valid.eq(cmp_sink.valid),
-                cmp_sink.ready.eq(cmp_source.ready),
-                If(cmp_sink.valid & cmp_sink.last & cmp_sink.ready,
-                    NextState("IDLE")
-                )
-            )
-        )
-        cmp_fsm.act("UPDATE_TAG_FIFO",
-            tag_fifo.we.eq(1),
-            tag_fifo.din.eq(cmp_sink.tag),
             cmp_source.valid.eq(cmp_sink.valid),
             cmp_sink.ready.eq(cmp_source.ready),
-            If(cmp_sink.valid & cmp_sink.ready,
+            If(cmp_sink.valid & cmp_sink.last & cmp_sink.ready,
+                If(cmp_sink.end,
+                    tags.sink.valid.eq(1),
+                    tags.sink.data.eq(cmp_sink.tag)
+                ),
                 NextState("IDLE")
             )
         )
--- litepcie/core/tlp/reordering.py
+++ litepcie/core/tlp/reordering.py
@@ -7,46 +7,39 @@ from litepcie.core.tlp.common import *
 
 class LitePCIeTLPReordering(Module):
     def __init__(self, data_width, max_pending_requests):
-        self.sink = stream.Endpoint(completion_layout(data_width))
-        self.source = stream.Endpoint(completion_layout(data_width))
+        self.sink = sink = stream.Endpoint(completion_layout(data_width))
+        self.source = source = stream.Endpoint(completion_layout(data_width))
 
-        self.req_we = Signal()
-        self.req_tag = Signal(log2_int(max_pending_requests))
+        self.tag = tag = stream.Endpoint([("data", log2_int(max_pending_requests))])
 
         # # #
 
-        tag_buffer = SyncFIFO([("data", log2_int(max_pending_requests))],
-                              8*max_pending_requests,
-                              buffered=True)
-        self.submodules += tag_buffer
+        # Store tags in the order read requests were emitted to the Host.
+        tags = SyncFIFO(
+            [("data", log2_int(max_pending_requests))],
+            max_pending_requests)
+        self.submodules += tags
+        self.comb += tag.connect(tags.sink)
+
+        # Completions data buffers
+        completion_write_cases = {}
+        completion_read_cases = {}
+        for i in range(max_pending_requests):
+            completion = SyncFIFO(
+                completion_layout(data_width),
+                2*max_request_size//(data_width//8))
+            self.submodules += completion
+            completion_write_cases[i] = [sink.connect(completion.sink)]
+            completion_read_cases[i] = [completion.source.connect(source)]
+
+        # Completions are written to the buffer indicated by the incoming tag.
+        completion_write_cases["default"] = [sink.ready.eq(1)]
+        self.comb += Case(sink.tag, completion_write_cases)
+
+        # Completions are read in the order the tags were emitted to the Host.
         self.comb += [
-            tag_buffer.sink.valid.eq(self.req_we),
-            tag_buffer.sink.data.eq(self.req_tag)
-        ]
-
-        reorder_buffers = []
-        for i in range(max_pending_requests):
-            reorder_buffer = SyncFIFO(completion_layout(data_width),
-                                      8*max_request_size//(data_width//8),
-                                      buffered=True)
-            reorder_buffers.append(reorder_buffer)
-        self.submodules += iter(reorder_buffers)
-
-        # store incoming completion in "sink.tag" buffer
-        cases = {}
-        for i in range(max_pending_requests):
-            cases[i] = [self.sink.connect(reorder_buffers[i].sink)]
-        cases["default"] = [self.sink.ready.eq(1)]
-        self.comb += Case(self.sink.tag, cases)
-
-        # read buffer according to tag_buffer order
-        cases = {}
-        for i in range(max_pending_requests):
-            cases[i] = [reorder_buffers[i].source.connect(self.source)]
-        cases["default"] = []
-        self.comb += [
-            Case(tag_buffer.source.data, cases),
-            If(self.source.valid & self.source.last & self.source.end,
-                tag_buffer.source.ready.eq(self.source.ready)
+            Case(tags.source.data, completion_read_cases),
+            If(source.valid & source.last & source.end,
+                tags.source.ready.eq(source.ready)
             )
         ]
