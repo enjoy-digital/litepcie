de469d39f53b2315aa2063700e07e555af800392
Florent Kermarrec <florent@enjoy-digital.fr>
2019-11-04 08:49:11 +0100

    phy/c5pciephy: improve readability with aligning

--- litepcie/phy/c5pciephy.py
+++ litepcie/phy/c5pciephy.py
@@ -15,15 +15,15 @@ from litepcie.common import *
 
 class C5PCIEPHY(Module, AutoCSR):
     def __init__(self, platform, pads, data_width=64, bar0_size=1*MB, cd="sys"):
-         # Streams ---------------------------------------------------------------------------------
-        self.sink = stream.Endpoint(phy_layout(data_width))
+        # Streams ---------------------------------------------------------------------------------
+        self.sink   = stream.Endpoint(phy_layout(data_width))
         self.source = stream.Endpoint(phy_layout(data_width))
-        self.msi = stream.Endpoint(msi_layout())
+        self.msi    = stream.Endpoint(msi_layout())
 
         # Parameters/Locals ------------------------------------------------------------------------
-        self.pads = pads
-        self.platform = platform
-        self.data_width = data_width
+        self.pads             = pads
+        self.platform         = platform
+        self.data_width       = data_width
 
         self.id               = Signal(16)
         self.bar0_size        = bar0_size
@@ -35,17 +35,17 @@ class C5PCIEPHY(Module, AutoCSR):
 
         # # #
 
-        pcie_clk = Signal()
-        pcie_rst_n = Signal(reset=1)
-        pcie_reconfig_clk = Signal()
-        pcie_coreclkout_hip_clk = Signal()
-        pcie_pld_clk_clk = Signal()
-        pcie_pld_clk_1_clk = Signal()
+        pcie_clk                       = Signal()
+        pcie_rst_n                     = Signal(reset=1)
+        pcie_reconfig_clk              = Signal()
+        pcie_coreclkout_hip_clk        = Signal()
+        pcie_pld_clk_clk               = Signal()
+        pcie_pld_clk_1_clk             = Signal()
 
-        pcie_config_tl_tl_cfg_add = Signal(4)
-        pcie_o_config_tl_tl_cfg_ctl = Signal(32)
+        pcie_config_tl_tl_cfg_add      = Signal(4)
+        pcie_o_config_tl_tl_cfg_ctl    = Signal(32)
         pcie_hip_rst_serdes_pll_locked = Signal()
-        pcie_o_power_mngt_pme_to_sr = Signal()
+        pcie_o_power_mngt_pme_to_sr    = Signal()
 
          # Clocking ---------------------------------------------------------------------------------
         pcie_refclk = Signal()
@@ -62,8 +62,8 @@ class C5PCIEPHY(Module, AutoCSR):
         else:
             tx_buffer = stream.Buffer(phy_layout(data_width))
             tx_buffer = ClockDomainsRenamer(cd)(tx_buffer)
-            tx_cdc = stream.AsyncFIFO(phy_layout(data_width), 32)
-            tx_cdc = ClockDomainsRenamer({"write": cd, "read": "pcie"})(tx_cdc)
+            tx_cdc    = stream.AsyncFIFO(phy_layout(data_width), 32)
+            tx_cdc    = ClockDomainsRenamer({"write": cd, "read": "pcie"})(tx_cdc)
             self.submodules += tx_buffer, tx_cdc
             self.comb += [
                 self.sink.connect(tx_buffer.sink),
@@ -75,8 +75,8 @@ class C5PCIEPHY(Module, AutoCSR):
         if cd == "pcie":
             rx_st = self.source
         else:
-            rx_cdc = stream.AsyncFIFO(phy_layout(data_width), 32)
-            rx_cdc = ClockDomainsRenamer({"write": "pcie", "read": cd})(rx_cdc)
+            rx_cdc    = stream.AsyncFIFO(phy_layout(data_width), 32)
+            rx_cdc    = ClockDomainsRenamer({"write": "pcie", "read": cd})(rx_cdc)
             rx_buffer = stream.Buffer(phy_layout(data_width))
             rx_buffer = ClockDomainsRenamer(cd)(rx_buffer)
             self.submodules += rx_buffer, rx_cdc
@@ -105,20 +105,20 @@ class C5PCIEPHY(Module, AutoCSR):
                 value = value*2
             return Case(command, cases)
 
-        bus_number = Signal(8)
-        device_number = Signal(5)
+        bus_number      = Signal(8)
+        device_number   = Signal(5)
         function_number = Signal(3)
-        dcommand = Signal(16)
+        dcommand        = Signal(16)
 
-        self.bus_number = bus_number
-        self.device_number = device_number
+        self.bus_number      = bus_number
+        self.device_number   = device_number
         self.function_number = function_number
 
-        tl_cfg_add_reg_lsb = Signal()
-        tl_cfg_add_reg2_lsb = Signal()
-        cfgctl_addr_change = Signal()
-        cfgctl_addr_change2 = Signal()
-        cfgctl_addr_strobe = Signal()
+        tl_cfg_add_reg_lsb    = Signal()
+        tl_cfg_add_reg2_lsb   = Signal()
+        cfgctl_addr_change    = Signal()
+        cfgctl_addr_change2   = Signal()
+        cfgctl_addr_strobe    = Signal()
         captured_cfg_addr_reg = Signal(4)
         captured_cfg_data_reg = Signal(32)
 
@@ -181,60 +181,60 @@ class C5PCIEPHY(Module, AutoCSR):
         # Hard IP ----------------------------------------------------------------------------------
         self.pcie_phy_params = dict(
             # Clocks
-            i_refclk_clk=pcie_refclk,
-            i_pld_clk_clk=ClockSignal("pcie"),
-            o_coreclkout_hip_clk=ClockSignal("pcie"),
+            i_refclk_clk                = pcie_refclk,
+            i_pld_clk_clk               = ClockSignal("pcie"),
+            o_coreclkout_hip_clk        = ClockSignal("pcie"),
 
             # Resets
-            i_npor_npor=1 if not hasattr(pads, "rst_n") else pads.rst_n,
-            i_npor_pin_perst=1 if not hasattr(pads, "rst_n") else pads.rst_n,
+            i_npor_npor                 = 1 if not hasattr(pads, "rst_n") else pads.rst_n,
+            i_npor_pin_perst            = 1 if not hasattr(pads, "rst_n") else pads.rst_n,
 
             # Hard IP Reconfiguration
-            i_reconfig_clk_clk=pcie_reconfig_clk,
-            i_reconfig_reset_reset_n=pcie_rst_n,
+            i_reconfig_clk_clk          = pcie_reconfig_clk,
+            i_reconfig_reset_reset_n    = pcie_rst_n,
 
             # Power Management
-            i_power_mngt_pme_to_cr=pcie_o_power_mngt_pme_to_sr,
-            o_power_mngt_pme_to_sr=pcie_o_power_mngt_pme_to_sr,
+            i_power_mngt_pme_to_cr      = pcie_o_power_mngt_pme_to_sr,
+            o_power_mngt_pme_to_sr      = pcie_o_power_mngt_pme_to_sr,
 
             # Config (Configuration space)
-            o_config_tl_tl_cfg_ctl=pcie_o_config_tl_tl_cfg_ctl,
-            o_config_tl_tl_cfg_add= pcie_config_tl_tl_cfg_add,
+            o_config_tl_tl_cfg_ctl      = pcie_o_config_tl_tl_cfg_ctl,
+            o_config_tl_tl_cfg_add      = pcie_config_tl_tl_cfg_add,
 
              # Control
-            o_hip_rst_serdes_pll_locked=pcie_hip_rst_serdes_pll_locked,
-            i_hip_rst_pld_core_ready=pcie_hip_rst_serdes_pll_locked,
+            o_hip_rst_serdes_pll_locked = pcie_hip_rst_serdes_pll_locked,
+            i_hip_rst_pld_core_ready    = pcie_hip_rst_serdes_pll_locked,
 
             # RX Port
-            o_rx_st_valid=rx_avst.valid,
-            o_rx_st_startofpacket=rx_avst.first,
-            o_rx_st_endofpacket=rx_avst.last,
-            i_rx_st_ready=rx_avst.ready,
-            o_rx_st_data=rx_avst.dat,
+            o_rx_st_valid               = rx_avst.valid,
+            o_rx_st_startofpacket       = rx_avst.first,
+            o_rx_st_endofpacket         = rx_avst.last,
+            i_rx_st_ready               = rx_avst.ready,
+            o_rx_st_data                = rx_avst.dat,
 
             # TX Port
-            i_tx_st_valid=tx_avst.valid,
-            i_tx_st_startofpacket=tx_avst.first,
-            i_tx_st_endofpacket=tx_avst.last,
-            o_tx_st_ready=tx_avst.ready,
-            i_tx_st_data=tx_avst.dat,
+            i_tx_st_valid               = tx_avst.valid,
+            i_tx_st_startofpacket       = tx_avst.first,
+            i_tx_st_endofpacket         = tx_avst.last,
+            o_tx_st_ready               = tx_avst.ready,
+            i_tx_st_data                = tx_avst.dat,
 
             # Serial IF
-            i_hip_serial_rx_in0=pads.rx_p[0],
-            i_hip_serial_rx_in1=pads.rx_p[1],
-            i_hip_serial_rx_in2=pads.rx_p[2],
-            i_hip_serial_rx_in3=pads.rx_p[3],
-            o_hip_serial_tx_out0=pads.tx_p[0],
-            o_hip_serial_tx_out1=pads.tx_p[1],
-            o_hip_serial_tx_out2=pads.tx_p[2],
-            o_hip_serial_tx_out3=pads.tx_p[3],
+            i_hip_serial_rx_in0         = pads.rx_p[0],
+            i_hip_serial_rx_in1         = pads.rx_p[1],
+            i_hip_serial_rx_in2         = pads.rx_p[2],
+            i_hip_serial_rx_in3         = pads.rx_p[3],
+            o_hip_serial_tx_out0        = pads.tx_p[0],
+            o_hip_serial_tx_out1        = pads.tx_p[1],
+            o_hip_serial_tx_out2        = pads.tx_p[2],
+            o_hip_serial_tx_out3        = pads.tx_p[3],
 
             # MSI
-            i_int_msi_app_msi_num=0,
-            i_int_msi_app_msi_req=cfg_msi.valid,
-            i_int_msi_app_msi_tc=0,
-            o_int_msi_app_msi_ack=cfg_msi.ready,
-            i_int_msi_app_int_sts=0
+            i_int_msi_app_msi_num       = 0,
+            i_int_msi_app_msi_req       = cfg_msi.valid,
+            i_int_msi_app_msi_tc        = 0,
+            o_int_msi_app_msi_ack       = cfg_msi.ready,
+            i_int_msi_app_int_sts       = 0
         )
 
     # External Hard IP -----------------------------------------------------------------------------
