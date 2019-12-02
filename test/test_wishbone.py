1ecc5487934c42a2a81534696fced973696be186
Florent Kermarrec <florent@enjoy-digital.fr>
2019-12-02 10:06:36 +0100

    test/test_wishbone: cleanup, add comments

--- test/test_wishbone.py
+++ test/test_wishbone.py
@@ -5,8 +5,6 @@ import unittest
 
 from migen import *
 
-from litex.gen.sim import *
-
 from litex.soc.interconnect import wishbone
 from litex.soc.interconnect.stream_sim import seed_to_data
 
@@ -15,48 +13,47 @@ from litepcie.frontend.wishbone import LitePCIeWishboneBridge
 
 from test.model.host import *
 
-root_id = 0x100
+# In this high level test, LitePCIeEndpoint is connected to LitePCIeWishboneBridge frontend, itself
+# connected to a Wishbone SRAM and our Host software model is used to generate Write/Read TLPs.
+#
+# The test verifies that the Host model is able to access the wishbone SRAM correctly through the
+# LitePCIeEndpoint.
+
+root_id     = 0x100
 endpoint_id = 0x400
-
-
-class DUT(Module):
-    def __init__(self, data_width):
-        self.submodules.host = Host(data_width, root_id, endpoint_id,
-            phy_debug=False,
-            chipset_debug=False,
-            host_debug=False)
-        self.submodules.endpoint = LitePCIeEndpoint(self.host.phy)
-
-        self.submodules.wishbone_bridge = LitePCIeWishboneBridge(self.endpoint, lambda a: 1)
-        self.submodules.sram = wishbone.SRAM(1024, bus=self.wishbone_bridge.wishbone)
-
-
-wr_datas = [seed_to_data(i, True) for i in range(64)]
-rd_datas = []
-
-
-def main_generator(dut):
-    for i in range(64):
-        yield from dut.host.chipset.wr32(i, [wr_datas[i]])
-
-    rd_datas.clear()
-    for i in range(64):
-        yield from dut.host.chipset.rd32(i)
-        rd_datas.append(dut.host.chipset.rd32_data[0])
-
 
 class TestWishbone(unittest.TestCase):
     def wishbone_test(self, data_width):
+        wr_datas = [seed_to_data(i, True) for i in range(64)]
+        rd_datas = []
+
+        def main_generator(dut, ndatas=64):
+            # Write ndatas to the Wishbone SRAM
+            for i in range(ndatas):
+                yield from dut.host.chipset.wr32(i, [wr_datas[i]])
+            # Read ndatas from the Wishbone SRAM
+            for i in range(ndatas):
+                yield from dut.host.chipset.rd32(i)
+                rd_datas.append(dut.host.chipset.rd32_data[0])
+
+        class DUT(Module):
+            def __init__(self, data_width):
+                self.submodules.host            = Host(data_width, root_id, endpoint_id)
+                self.submodules.endpoint        = LitePCIeEndpoint(self.host.phy)
+                self.submodules.wishbone_bridge = LitePCIeWishboneBridge(self.endpoint, lambda a: 1)
+                self.submodules.sram            = wishbone.SRAM(128, bus=self.wishbone_bridge.wishbone)
+
         dut = DUT(data_width)
         generators = {
             "sys" : [
                 main_generator(dut),
                 dut.host.chipset.phy.phy_sink.generator(),
-                dut.host.chipset.phy.phy_source.generator()
+                dut.host.chipset.phy.phy_source.generator(),
             ]
         }
         clocks = {"sys": 10}
         run_simulation(dut, generators, clocks)
+        # Verify Write/Read datas match
         self.assertEqual(wr_datas, rd_datas)
 
     def test_wishbone_64b(self):
