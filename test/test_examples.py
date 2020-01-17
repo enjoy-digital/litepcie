06ee416e9eedf6e44ac13b89d06c1c96d9c3dd39
Florent Kermarrec <florent@enjoy-digital.fr>
2020-01-17 11:43:04 +0100

    test/test_examples: update

--- test/test_examples.py
+++ test/test_examples.py
@@ -1,36 +1,28 @@
-# This file is Copyright (c) 2019 Florent Kermarrec <florent@enjoy-digital.fr>
+# This file is Copyright (c) 2019-2020 Florent Kermarrec <florent@enjoy-digital.fr>
 # License: BSD
 
 import unittest
 import os
 
-root_dir    = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..")
-make_script = os.path.join(root_dir, "examples", "make.py")
-
 class TestExamples(unittest.TestCase):
-    def example_test(self, t, s):
-        os.system("rm -rf {}/build".format(root_dir))
-        cmd = "python3 " + make_script + " "
-        cmd += "-t {} ".format(t)
-        cmd += "-s {} ".format(s)
-        cmd += "-p kc705 "
-        cmd += "-Ob run False "
-        cmd += "build-bitstream"
-        os.system(cmd)
-        self.assertEqual(os.path.isfile("{}/build/{}_kc705.v".format(root_dir, s.lower())), True)
-        self.assertEqual(os.path.isfile("{}/build/csr.h".format(root_dir, s.lower())), True)
-        self.assertEqual(os.path.isfile("{}/build/soc.h".format(root_dir, s.lower())), True)
+    def target_test(self, target):
+        os.system("rm -rf examples/build")
+        os.system("cd examples && python3 {}.py".format(target))
+        self.assertEqual(os.path.isfile("examples/build/gateware/{}.v".format(target)), True)
+        self.assertEqual(os.path.isfile("examples/build/csr.h"), True)
+        self.assertEqual(os.path.isfile("examples/build/soc.h"), True)
+        self.assertEqual(os.path.isfile("examples/build/mem.h"), True)
 
-    def test_dma_example(self):
-        self.example_test("dma", "PCIeDMASoC")
+    def test_kc705_target(self):
+        self.target_test("kc705")
 
     def gen_test(self, name):
         os.system("rm -rf examples/build")
-        os.system("cd examples && python3 ../litepcie/gen.py gen/{}.yml".format(name))
+        os.system("cd examples && python3 ../litepcie/gen.py {}.yml".format(name))
         errors = not os.path.isfile("examples/build/gateware/litepcie_core.v")
         os.system("rm -rf examples/build")
         return errors
 
-    def test_gen_ac701(self):
+    def test_ac701_gen(self):
         errors = self.gen_test("ac701")
         self.assertEqual(errors, 0)
