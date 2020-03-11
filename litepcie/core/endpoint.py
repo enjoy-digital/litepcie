b3c07d3fec88e7273d50eb4b1af614f54ef1d405
Florent Kermarrec <florent@enjoy-digital.fr>
2020-03-11 19:31:29 +0100

    core/endpoint: add support for separate request/completion channels on the PHY.
    On 7-Series and Cyclone5, a single AXI/AvalonST stream is used for both requests/completions.
    On Ultrascale(+), separate AXI streams are available for requests/completions.

--- litepcie/core/endpoint.py
+++ litepcie/core/endpoint.py
@@ -1,4 +1,4 @@
-# This file is Copyright (c) 2015-2019 Florent Kermarrec <florent@enjoy-digital.fr>
+# This file is Copyright (c) 2015-2020 Florent Kermarrec <florent@enjoy-digital.fr>
 # License: BSD
 
 from migen import *
@@ -19,14 +19,40 @@ class LitePCIeEndpoint(Module):
         # # #
 
         # TLP Packetizer / Depacketizer ------------------------------------------------------------
-        depacketizer = LitePCIeTLPDepacketizer(phy.data_width, endianness, phy.bar0_mask)
-        packetizer   = LitePCIeTLPPacketizer(phy.data_width, endianness)
-        self.submodules.depacketizer = depacketizer
-        self.submodules.packetizer   = packetizer
-        self.comb += [
-            phy.source.connect(depacketizer.sink),
-            packetizer.source.connect(phy.sink)
-        ]
+        if hasattr(phy, "sink") and hasattr(phy, "source"):
+            # Shared Request/Completion channels
+            depacketizer = LitePCIeTLPDepacketizer(phy.data_width, endianness, phy.bar0_mask)
+            packetizer   = LitePCIeTLPPacketizer(phy.data_width, endianness)
+            self.submodules.depacketizer = depacketizer
+            self.submodules.packetizer   = packetizer
+            self.comb += [
+                phy.source.connect(depacketizer.sink),
+                packetizer.source.connect(phy.sink)
+            ]
+            req_source = depacketizer.req_source
+            cmp_sink   = packetizer.cmp_sink
+            req_sink   = packetizer.req_sink
+            cmp_source = depacketizer.cmp_source
+        else:
+            # Separate Request/Completion channels
+            cmp_depacketizer = LitePCIeTLPDepacketizer(phy.data_width, endianness, phy.bar0_mask)
+            req_depacktizer  = LitePCIeTLPDepacketizer(phy.data_width, endianness, phy.bar0_mask)
+            cmp_packetizer   = LitePCIeTLPPacketizer(phy.data_width, endianness)
+            req_packetizer   = LitePCIeTLPPacketizer(phy.data_width, endianness)
+            self.submodules.cmp_depacketizer = cmp_depacketizer
+            self.submodules.req_depacketizer = req_depacketizer
+            self.submodules.cmp_packetizer   = cmp_packetizer
+            self.submodules.req_packetizer   = req_packetizer
+            self.comb += [
+                phy.cmp_source.connect(cmp_depacketizer.sink),
+                phy.req_source.connect(req_depacketizer.sink),
+                cmp_packetizer.source.connect(phy.cmp_sink),
+                req_packetizer.source.connect(phy.req_sink),
+            ]
+            req_source = req_depacketizer.req_source
+            cmp_sink   = cmp_packetizer.cmp_sink
+            req_sink   = req_packetizer.req_sink
+            cmp_source = cmp_depacketizer.cmp_source
 
         # Crossbar ---------------------------------------------------------------------------------
         crossbar = LitePCIeCrossbar(phy.data_width, max_pending_requests)
@@ -34,12 +60,12 @@ class LitePCIeEndpoint(Module):
 
         # Slave: HOST initiates the transactions ---------------------------------------------------
         self.comb += [
-            depacketizer.req_source.connect(crossbar.phy_slave.sink),
-            crossbar.phy_slave.source.connect(packetizer.cmp_sink)
+            req_source.connect(crossbar.phy_slave.sink),
+            crossbar.phy_slave.source.connect(cmp_sink),
         ]
 
         # Master: FPGA initiates the transactions --------------------------------------------------
         self.comb += [
-            crossbar.phy_master.source.connect(packetizer.req_sink),
-            depacketizer.cmp_source.connect(crossbar.phy_master.sink)
+            crossbar.phy_master.source.connect(req_sink),
+            cmp_source.connect(crossbar.phy_master.sink),
         ]
