ad93d520af7f48a27f1a435f95c3bafb515226c6
Florent Kermarrec <florent@enjoy-digital.fr>
2020-06-03 08:19:30 +0200

    software: add generate_litepcie_software_headers/software functions to avoid duplication in targets.

--- litepcie/software/__init__.py
+++ litepcie/software/__init__.py
@@ -1,6 +1,23 @@
 import os
 from distutils.dir_util import copy_tree
 
+from litex.build import tools
+
+from litex.soc.integration.export import get_csr_header, get_soc_header, get_mem_header
+
+
 def copy_litepcie_software(dst):
 	src = os.path.abspath(os.path.dirname(__file__))
 	copy_tree(src, dst)
+
+def generate_litepcie_software_headers(soc, dst):
+    csr_header = get_csr_header(soc.csr_regions, soc.constants, with_access_functions=False)
+    tools.write_to_file(os.path.join(dst, "kernel", "csr.h"), csr_header)
+    soc_header = get_soc_header(soc.constants, with_access_functions=False)
+    tools.write_to_file(os.path.join(dst, "kernel", "soc.h"), soc_header)
+    mem_header = get_mem_header(soc.mem_regions)
+    tools.write_to_file(os.path.join(dst, "kernel", "mem.h"), mem_header)
+
+def generate_litepcie_software(soc, dst):
+	copy_litepcie_software(dst)
+	generate_litepcie_software_headers(soc, dst)
