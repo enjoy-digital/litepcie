de72c089df8700316dc3621be284f0193610ed74
Florent Kermarrec <florent@enjoy-digital.fr>
2021-09-09 17:58:34 +0200

    tlp/controller: Cleanup/Simplify and add comments, no functional change.

--- litepcie/tlp/controller.py
+++ litepcie/tlp/controller.py
@@ -1,7 +1,7 @@
 #
 # This file is part of LitePCIe.
 #
-# Copyright (c) 2015-2019 Florent Kermarrec <florent@enjoy-digital.fr>
+# Copyright (c) 2015-2021 Florent Kermarrec <florent@enjoy-digital.fr>
 # SPDX-License-Identifier: BSD-2-Clause
 
 from migen import *
@@ -17,103 +17,141 @@ class LitePCIeTLPController(Module):
     Arbitrate/throttle TLP requests and reorder/assemble/redirect completions.
     """
     def __init__(self, data_width, max_pending_requests, cmp_bufs_buffered=True):
-        self.master_in = LitePCIeMasterInternalPort(data_width)
+        self.master_in  = LitePCIeMasterInternalPort(data_width)
         self.master_out = LitePCIeMasterInternalPort(data_width)
 
         # # #
 
-        req_sink, req_source = self.master_in.sink, self.master_out.sink
+        req_sink, req_source = self.master_in.sink,    self.master_out.sink
         cmp_sink, cmp_source = self.master_out.source, self.master_in.source
 
-        tags_bits = log2_int(max_pending_requests)
+        # Parameters.
+        tag_bits = log2_int(max_pending_requests)
 
-        # Tags queue -------------------------------------------------------------------------------
-        # The tags queue is filled initially with the tags that will be used to issue read requests
-        # to the host.A tag is dequeued when a read requests is issued to the host and queued when
+        # Tag queue --------------------------------------------------------------------------------
+        # The tag queue is filled initially with the tags that will be used to issue read requests
+        # to the host. A tag is dequeued when a read requests is issued to the host and queued when
         # a readcomplementation is received from the host.
-        tags_queue = SyncFIFO([("tag", tags_bits)], max_pending_requests, buffered=True)
-        self.submodules.tags_queue = tags_queue
+        tag_queue = SyncFIFO([("tag", tag_bits)], max_pending_requests, buffered=True)
+        self.submodules.tag_queue = tag_queue
 
         # Requests queue ---------------------------------------------------------------------------
         # Store the read requests tags as emitted to the host, datas will be dequeued in this order
-        requests_queue_layout = [("tag", tags_bits), ("channel", 8), ("user_id", 8)]
-        requests_queue = SyncFIFO(requests_queue_layout, max_pending_requests, buffered=True)
-        self.submodules.requests_queue = requests_queue
+        req_queue_layout = [("tag", tag_bits), ("channel", 8), ("user_id", 8)]
+        req_queue        = SyncFIFO(req_queue_layout, max_pending_requests, buffered=True)
+        self.submodules.req_queue = req_queue
 
-        # Requests Managment -----------------------------------------------------------------------
-        self.comb += req_sink.connect(req_source, omit=set(["valid", "ready", "tag"]))
-        self.submodules.req_fsm = req_fsm = FSM(reset_state="IDLE")
-        req_fsm.act("IDLE",
+        # Requests Management ----------------------------------------------------------------------
+
+        # Connect Data-Path.
+        self.comb += req_sink.connect(req_source, omit={"valid", "ready", "tag"})
+        self.submodules.req_fsm = req_fsm = FSM(reset_state="WAIT-REQ")
+
+        # FSM.
+        req_fsm.act("WAIT-REQ",
+            # Wait for a TLP Request...
             If(req_sink.valid & req_sink.first,
+                # TLP Write: We can send the request directly.
                 If(req_sink.we,
-                   NextState("WRITE-REQUEST")
-                ).Elif(tags_queue.source.valid & requests_queue.sink.ready,
-                   NextState("READ-REQUEST")
+                   NextState("SEND-WRITE-REQ")
+                # TLP Read:  We can send the request when one tag available and space in req_queue.
+                ).Elif(tag_queue.source.valid & req_queue.sink.ready,
+                   NextState("SEND-READ-REQ")
                 )
             )
         )
-        req_fsm.act("WRITE-REQUEST",
-            req_source.valid.eq(req_sink.valid),
-            req_sink.ready.eq(req_source.ready),
+        req_fsm.act("SEND-WRITE-REQ",
+            # Connect Control-Path.
+            req_sink.connect(req_source, keep={"valid", "ready"}),
             req_source.tag.eq(32),
+            # End Request and return to Wait on last valid cycle.
             If(req_source.valid & req_source.ready & req_source.last,
-                NextState("IDLE")
+                NextState("WAIT-REQ")
             )
         )
-        self.comb += [
-            requests_queue.sink.tag.eq(tags_queue.source.tag),
-            requests_queue.sink.channel.eq(req_sink.channel),
-            requests_queue.sink.user_id.eq(req_sink.user_id)
-        ]
-        req_fsm.act("READ-REQUEST",
-            req_source.valid.eq(req_sink.valid),
-            req_source.tag.eq(tags_queue.source.tag),
-            req_sink.ready.eq(req_source.ready),
+        req_fsm.act("SEND-READ-REQ",
+            # Connect Control-Path.
+            req_sink.connect(req_source, keep={"valid", "ready"}),
+            req_source.tag.eq(tag_queue.source.tag),
+            # End Request and return to Wait on last valid cycle.
             If(req_source.valid & req_source.ready & req_source.last,
-                tags_queue.source.ready.eq(1),
-                requests_queue.sink.valid.eq(1),
-                NextState("IDLE")
+                # Pop Tag from tag_queue.
+                tag_queue.source.ready.eq(1),
+                # Push Req to req_queue.
+                req_queue.sink.valid.eq(1),
+                req_queue.sink.tag.eq(tag_queue.source.tag),
+                req_sink.connect(req_queue.sink, keep={"channel", "user_id"}),
+                NextState("WAIT-REQ")
             )
         )
 
-        # Completions data buffers (Reordering) ----------------------------------------------------
+        # Completions Data Buffers (Reordering) ----------------------------------------------------
+        #                          ┌───────────────┐
+        #   Read Requests'Tags ────►   Req Queue   ├───┐
+        #                          └───────────────┘   │
+        #                                              │
+        #                                              ▼
+        #                 ┌─► Demux                   Mux
+        #             Tag │   ┌──┐    ┌──────────┐   ┌───┐
+        #                 │   │  ├────► Buffer 0 ├───►   │
+        #                 │   │  │    └──────────┘   │   │
+        #                 │   │  │                   │   │
+        #                 │   │  │    ┌──────────┐   │   │
+        #        Cmp In ──┴───►  ├────► Buffer . ├───►   ├─────► Cmp Out
+        #                     │  │    └──────────┘   │   │
+        #                     │  │                   │   │
+        #                     │  │    ┌──────────┐   │   │
+        #                     │  ├────► Buffer N ├───►   │
+        #                     └──┘    └──────────┘   └───┘
+        #
+        #
+
         cmp_reorder = stream.Endpoint(completion_layout(data_width))
-        cmp_write_cases = {}
-        cmp_read_cases = {}
+        cmp_bufs    = []
+
+        # Create Buffers.
         for i in range(max_pending_requests):
-            cmp_buf_depth = int(4*max_request_size/(data_width/8))
-            cmp_buf = SyncFIFO(completion_layout(data_width), cmp_buf_depth, buffered=cmp_bufs_buffered)
-            self.submodules += cmp_buf
-            cmp_write_cases[i] = [cmp_reorder.connect(cmp_buf.sink)]
-            cmp_read_cases[i] = [cmp_buf.source.connect(cmp_source)]
+            cmp_buf_depth = 4*max_request_size//(data_width//8)
+            print(cmp_buf_depth)
+            cmp_buf       = SyncFIFO(completion_layout(data_width), cmp_buf_depth, buffered=cmp_bufs_buffered)
+            cmp_bufs.append(cmp_buf)
+        self.submodules += cmp_bufs
 
-        # Completions are written to the buffer indicated by the incoming tag.
-        cmp_write_cases["default"] = [cmp_reorder.ready.eq(1)]
-        self.comb += Case(cmp_reorder.tag, cmp_write_cases)
+        # Connect Cmp Input to Buffers (based on incoming Tag).
+        self.comb += Case(cmp_reorder.tag,
+            {i: cmp_reorder.connect(cmp_bufs[i].sink) for i in range(len(cmp_bufs))})
 
-        # Completions are read in the order the tags were emitted to the host.
+        # Connect Buffers to Cmp Output (based on Tag from req_ueue).
+        self.comb += Case(req_queue.source.tag,
+            {i: cmp_bufs[i].source.connect(cmp_source) for i in range(len(cmp_bufs))})
         self.comb += [
-            Case(requests_queue.source.tag, cmp_read_cases),
+            # Pop Req from req_queue when Req is fully received.
             If(cmp_source.valid & cmp_source.last & cmp_source.end,
-                requests_queue.source.ready.eq(cmp_source.ready)
+                req_queue.source.ready.eq(cmp_source.ready)
             ),
-            cmp_source.channel.eq(requests_queue.source.channel),
-            cmp_source.user_id.eq(requests_queue.source.user_id)
+            req_queue.source.connect(cmp_source, keep={"channel", "user_id"}),
         ]
 
-        # Completions Managment --------------------------------------------------------------------
-        self.comb += cmp_sink.connect(cmp_reorder, omit=set(["valid", "ready"]))
-        self.submodules.cmp_fsm = cmp_fsm = FSM(reset_state="FILL-TAGS-QUEUE")
-        fill_tag = Signal(tags_bits)
-        cmp_fsm.act("FILL-TAGS-QUEUE",
-            tags_queue.sink.valid.eq(1),
-            tags_queue.sink.tag.eq(fill_tag),
+        # Completions Management -------------------------------------------------------------------
+
+        fill_tag = Signal(tag_bits)
+
+        # Connect Data-Path.
+        self.comb += cmp_sink.connect(cmp_reorder, omit={"valid", "ready"})
+
+        # FSM
+        self.submodules.cmp_fsm = cmp_fsm = FSM(reset_state="FILL-TAG-QUEUE")
+        cmp_fsm.act("FILL-TAG-QUEUE",
+            # Pre-fill Tags.
+            tag_queue.sink.valid.eq(1),
+            tag_queue.sink.tag.eq(fill_tag),
             NextValue(fill_tag, fill_tag + 1),
             If(fill_tag == (max_pending_requests - 1),
                 NextState("WAIT")
             )
         )
         cmp_fsm.act("WAIT",
+            # Wait for a TLP Completion...
             If(cmp_sink.valid & cmp_sink.first,
                 NextState("RUN")
             ).Else(
@@ -121,12 +159,13 @@ class LitePCIeTLPController(Module):
             )
         )
         cmp_fsm.act("RUN",
-            cmp_reorder.valid.eq(cmp_sink.valid),
-            cmp_sink.ready.eq(cmp_reorder.ready),
+            # Connect Control-Path.
+            cmp_sink.connect(cmp_reorder, keep={"valid", "ready"}),
+            # Push incoming Tag to tag_queue when Cmp is fully received.
             If(cmp_sink.valid & cmp_sink.ready & cmp_sink.last,
                 If(cmp_sink.end,
-                    tags_queue.sink.valid.eq(1),
-                    tags_queue.sink.tag.eq(cmp_sink.tag)
+                    tag_queue.sink.valid.eq(1),
+                    tag_queue.sink.tag.eq(cmp_sink.tag)
                 ),
                 NextState("WAIT")
             )
