447e5c32536d3b6d43f1154c8808fc3af03a341b
Florent Kermarrec <florent@enjoy-digital.fr>
2022-01-25 14:45:19 +0100

    tlp/common: Cleanup Constants/Helpers.

--- litepcie/tlp/common.py
+++ litepcie/tlp/common.py
@@ -1,7 +1,7 @@
 #
 # This file is part of LitePCIe.
 #
-# Copyright (c) 2015-2019 Florent Kermarrec <florent@enjoy-digital.fr>
+# Copyright (c) 2015-2022 Florent Kermarrec <florent@enjoy-digital.fr>
 # SPDX-License-Identifier: BSD-2-Clause
 
 from migen import *
@@ -10,6 +10,9 @@ from litepcie.common import *
 
 # Constants ----------------------------------------------------------------------------------------
 
+max_payload_size = 512
+max_request_size = 512
+
 fmt_type_dict = {
     "mem_rd32": 0b0000000,
     "mem_wr32": 0b1000000,
@@ -17,75 +20,77 @@ fmt_type_dict = {
     "mem_wr64": 0b1100000,
 
     "cpld":     0b1001010,
-    "cpl":      0b0001010
+    "cpl":      0b0001010,
 }
 
 cpl_dict = {
     "sc":  0b000,
     "ur":  0b001,
     "crs": 0b010,
-    "ca":  0b011
+    "ca":  0b011,
 }
-
-max_payload_size = 512
-max_request_size = 512
 
 # Headers ------------------------------------------------------------------------------------------
 
 tlp_common_header_length = 16
 tlp_common_header_fields = {
-    "fmt":  HeaderField(0*4, 29, 2),
-    "type": HeaderField(0*4, 24, 5),
+    "fmt":  HeaderField(byte=0*4, offset=29, width=2),
+    "type": HeaderField(byte=0*4, offset=24, width=5),
 }
-tlp_common_header = Header(tlp_common_header_fields,
-                           tlp_common_header_length,
-                           swap_field_bytes=False)
-
+tlp_common_header = Header(
+    fields           = tlp_common_header_fields,
+    length           = tlp_common_header_length,
+    swap_field_bytes = False
+)
 
 tlp_request_header_length = 16
 tlp_request_header_fields = {
-    "fmt":          HeaderField(0*4, 29,  2),
-    "type":         HeaderField(0*4, 24,  5),
-    "tc":           HeaderField(0*4, 20,  3),
-    "td":           HeaderField(0*4, 15,  1),
-    "ep":           HeaderField(0*4, 14,  1),
-    "attr":         HeaderField(0*4, 12,  2),
-    "length":       HeaderField(0*4,  0, 10),
+    "fmt":          HeaderField(byte=0*4, offset=29, width= 2),
+    "type":         HeaderField(byte=0*4, offset=24, width= 5),
+    "tc":           HeaderField(byte=0*4, offset=20, width= 3),
+    "td":           HeaderField(byte=0*4, offset=15, width= 1),
+    "ep":           HeaderField(byte=0*4, offset=14, width= 1),
+    "attr":         HeaderField(byte=0*4, offset=12, width= 2),
+    "length":       HeaderField(byte=0*4, offset= 0, width=10),
 
-    "requester_id": HeaderField(1*4, 16, 16),
-    "tag":          HeaderField(1*4,  8,  8),
-    "last_be":      HeaderField(1*4,  4,  4),
-    "first_be":     HeaderField(1*4,  0,  4),
+    "requester_id": HeaderField(byte=1*4, offset=16, width=16),
+    "tag":          HeaderField(byte=1*4, offset= 8, width= 8),
+    "last_be":      HeaderField(byte=1*4, offset= 4, width= 4),
+    "first_be":     HeaderField(byte=1*4, offset= 0, width= 4),
 
-    "address":      HeaderField(2*4,  0, 32),
+    "address":      HeaderField(byte=2*4, offset= 0, width=32),
 }
-tlp_request_header = Header(tlp_request_header_fields,
-                            tlp_request_header_length,
-                            swap_field_bytes=False)
+tlp_request_header = Header(
+    fields           = tlp_request_header_fields,
+    length           = tlp_request_header_length,
+    swap_field_bytes = False
+)
 
 
 tlp_completion_header_length = 16
 tlp_completion_header_fields = {
-    "fmt":           HeaderField(0*4, 29,  2),
-    "type":          HeaderField(0*4, 24,  5),
-    "tc":            HeaderField(0*4, 20,  3),
-    "td":            HeaderField(0*4, 15,  1),
-    "ep":            HeaderField(0*4, 14,  1),
-    "attr":          HeaderField(0*4, 12,  2),
-    "length":        HeaderField(0*4,  0, 10),
+    "fmt":           HeaderField(byte=0*4, offset=29, width= 2),
+    "type":          HeaderField(byte=0*4, offset=24, width= 5),
+    "tc":            HeaderField(byte=0*4, offset=20, width= 3),
+    "td":            HeaderField(byte=0*4, offset=15, width= 1),
+    "ep":            HeaderField(byte=0*4, offset=14, width= 1),
+    "attr":          HeaderField(byte=0*4, offset=12, width= 2),
+    "length":        HeaderField(byte=0*4, offset= 0, width=10),
 
-    "completer_id":  HeaderField(1*4, 16, 16),
-    "status":        HeaderField(1*4, 13,  3),
-    "bcm":           HeaderField(1*4, 12,  1),
-    "byte_count":    HeaderField(1*4,  0, 12),
+    "completer_id":  HeaderField(byte=1*4, offset=16, width=16),
+    "status":        HeaderField(byte=1*4, offset=13, width= 3),
+    "bcm":           HeaderField(byte=1*4, offset=12, width= 1),
+    "byte_count":    HeaderField(byte=1*4, offset= 0, width=12),
 
-    "requester_id":  HeaderField(2*4, 16, 16),
-    "tag":           HeaderField(2*4,  8,  8),
-    "lower_address": HeaderField(2*4,  0,  7),
+    "requester_id":  HeaderField(byte=2*4, offset=16, width=16),
+    "tag":           HeaderField(byte=2*4, offset= 8, width= 8),
+    "lower_address": HeaderField(byte=2*4, offset= 0, width= 7),
 }
-tlp_completion_header = Header(tlp_completion_header_fields,
-                               tlp_completion_header_length,
-                               swap_field_bytes=False)
+tlp_completion_header = Header(
+    fields           = tlp_completion_header_fields,
+    length           = tlp_completion_header_length,
+    swap_field_bytes = False
+)
 
 # Helpers ------------------------------------------------------------------------------------------
 
