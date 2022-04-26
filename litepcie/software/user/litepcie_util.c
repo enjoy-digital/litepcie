18985c79ca7be1e05b00ec2bbe61372a59fd4221
Florent Kermarrec <florent@enjoy-digital.fr>
2022-04-26 19:54:12 +0200

    software/user/litepcie_util: Cleanup/Simplify and add comments.
    Also remove uart_test since liteuart driver is now integrated in driver and
    uart can be simply tested through litex_term /dev/ttyLXU0.

--- litepcie/software/user/litepcie_util.c
+++ litepcie/software/user/litepcie_util.c
@@ -3,7 +3,7 @@
  *
  * This file is part of LitePCIe.
  *
- * Copyright (C) 2018-2020 / EnjoyDigital  / florent@enjoy-digital.fr
+ * Copyright (C) 2018-2022 / Enjoy-Digital / florent@enjoy-digital.fr
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
@@ -17,8 +17,14 @@
 #include <signal.h>
 #include "liblitepcie.h"
 
-#define DMA_CHECK_DATA
-#define DMA_RANDOM_DATA
+/* Parameters */
+/*------------*/
+
+#define DMA_CHECK_DATA   /* Un-comment to disable data check */
+#define DMA_RANDOM_DATA  /* Un-comment to disable data random */
+
+/* Variables */
+/*-----------*/
 
 static char litepcie_device[1024];
 static int litepcie_device_num;
@@ -29,13 +35,14 @@ void intHandler(int dummy) {
     keep_running = 0;
 }
 
-/* info */
+/* Info */
+/*------*/
 
 static void info(void)
 {
     int fd;
     int i;
-    unsigned char fpga_identification[256];
+    unsigned char fpga_identifier[256];
 
     fd = open(litepcie_device, O_RDWR);
     if (fd < 0) {
@@ -43,30 +50,67 @@ static void info(void)
         exit(1);
     }
 
+
+    printf("\e[1m[> FPGA/SoC Information:\e[0m\n");
+    printf("------------------------\n");
+
     for (i = 0; i < 256; i ++)
-        fpga_identification[i] = litepcie_readl(fd, CSR_IDENTIFIER_MEM_BASE + 4 * i);
-    printf("FPGA identification: %s\n", fpga_identification);
+        fpga_identifier[i] = litepcie_readl(fd, CSR_IDENTIFIER_MEM_BASE + 4 * i);
+    printf("FPGA Identifier:  %s.\n", fpga_identifier);
 #ifdef CSR_DNA_BASE
-    printf("FPGA dna: 0x%08x%08x\n",
+    printf("FPGA DNA:         0x%08x%08x\n",
         litepcie_readl(fd, CSR_DNA_ID_ADDR + 4 * 0),
         litepcie_readl(fd, CSR_DNA_ID_ADDR + 4 * 1)
     );
 #endif
 #ifdef CSR_XADC_BASE
-    printf("FPGA temperature: %0.1f °C\n",
+    printf("FPGA Temperature: %0.1f °C\n",
            (double)litepcie_readl(fd, CSR_XADC_TEMPERATURE_ADDR) * 503.975/4096 - 273.15);
-    printf("FPGA vccint: %0.2f V\n",
+    printf("FPGA VCC-INT:     %0.2f V\n",
            (double)litepcie_readl(fd, CSR_XADC_VCCINT_ADDR) / 4096 * 3);
-    printf("FPGA vccaux: %0.2f V\n",
+    printf("FPGA VCC-AUX:     %0.2f V\n",
            (double)litepcie_readl(fd, CSR_XADC_VCCAUX_ADDR) / 4096 * 3);
-    printf("FPGA vccbram: %0.2f V\n",
+    printf("FPGA VCC-BRAM:    %0.2f V\n",
            (double)litepcie_readl(fd, CSR_XADC_VCCBRAM_ADDR) / 4096 * 3);
 #endif
     close(fd);
 }
 
+/* SCRATCH */
+/*---------*/
+
+void scratch_test(void)
+{
+    int fd;
+
+    printf("\e[1m[> Scratch register test:\e[0m\n");
+    printf("-------------------------\n");
+
+    /* Open LitePCIe device. */
+    fd = open(litepcie_device, O_RDWR);
+    if (fd < 0) {
+        fprintf(stderr, "Could not init driver\n");
+        exit(1);
+    }
+
+    /* Write to scratch register. */
+    printf("Write 0x12345678 to Scratch register:\n");
+    litepcie_writel(fd, CSR_CTRL_SCRATCH_ADDR, 0x12345678);
+    printf("Read: 0x%08x\n", litepcie_readl(fd, CSR_CTRL_SCRATCH_ADDR));
+
+    /* Read from scratch register. */
+    printf("Write 0xdeadbeef to Scratch register:\n");
+    litepcie_writel(fd, CSR_CTRL_SCRATCH_ADDR, 0xdeadbeef);
+    printf("Read: 0x%08x\n", litepcie_readl(fd, CSR_CTRL_SCRATCH_ADDR));
+
+    /* Close LitePCIe device. */
+    close(fd);
+}
+
+/* SPI Flash */
+/*-----------*/
+
 #ifdef CSR_FLASH_BASE
-/* flash */
 
 static void flash_progress(void *opaque, const char *fmt, ...)
 {
@@ -86,17 +130,18 @@ static void flash_program(uint32_t base, const uint8_t *buf1, int size1)
     int sector_size;
     int errors;
 
+    /* Open LitePCIe device. */
     fd = open(litepcie_device, O_RDWR);
     if (fd < 0) {
         fprintf(stderr, "Could not init driver\n");
         exit(1);
     }
 
+    /* Get flash sector size and pad size to it. */
     sector_size = litepcie_flash_get_erase_block_size(fd);
-
-    /* pad to sector_size */
     size = ((size1 + sector_size - 1) / sector_size) * sector_size;
 
+    /* Alloc buffer and copy data to it. */
     buf = calloc(1, size);
     if (!buf) {
         fprintf(stderr, "%d: alloc failed\n", __LINE__);
@@ -104,20 +149,18 @@ static void flash_program(uint32_t base, const uint8_t *buf1, int size1)
     }
     memcpy(buf, buf1, size1);
 
-    printf("Programming (%d bytes at 0x%08x)\n", size, base);
-
+    /* Program flash. */
+    printf("Programming (%d bytes at 0x%08x)...\n", size, base);
     errors = litepcie_flash_write(fd, buf, base, size, flash_progress, NULL);
-
-    /* result */
     if (errors) {
-        printf("Failed %d errors\n", errors);
+        printf("Failed %d errors.\n", errors);
         exit(1);
     } else {
-        printf("Success\n");
+        printf("Success.\n");
     }
 
+    /* Free buffer and close LitePCIe device. */
     free(buf);
-
     close(fd);
 }
 
@@ -127,12 +170,14 @@ static void flash_write(const char *filename, uint32_t offset)
     int size;
     FILE * f;
 
+    /* Open data source file. */
     f = fopen(filename, "rb");
     if (!f) {
         perror(filename);
         exit(1);
     }
 
+    /* Get size, alloc buffer and copy data to it. */
     fseek(f, 0L, SEEK_END);
     size = ftell(f);
     fseek(f, 0L, SEEK_SET);
@@ -144,11 +189,13 @@ static void flash_write(const char *filename, uint32_t offset)
     ssize_t ret = fread(data, size, 1, f);
     fclose(f);
 
+    /* Program file to flash */
     if (ret != 1)
         perror(filename);
     else
         flash_program(offset, data, size);
 
+    /* Free buffer */
     free(data);
 }
 
@@ -161,31 +208,36 @@ static void flash_read(const char *filename, uint32_t size, uint32_t offset)
     uint8_t byte;
     int i;
 
+    /* Open data destination file. */
     f = fopen(filename, "wb");
     if (!f) {
         perror(filename);
         exit(1);
     }
 
+    /* Open LitePCIe device. */
     fd = open(litepcie_device, O_RDWR);
     if (fd < 0) {
         fprintf(stderr, "Could not init driver\n");
         exit(1);
     }
 
-    base = offset;
+    /* Get flash sector size. */
     sector_size = litepcie_flash_get_erase_block_size(fd);
+
+    /* Read flash and write to destination file. */
+    base = offset;
     for (i = 0; i < size; i++) {
         if ((i % sector_size) == 0) {
-            printf("Dumping %08x\r", base + i);
+            printf("Reading 0x%08x\r", base + i);
             fflush(stdout);
         }
         byte = litepcie_flash_read(fd, base + i);
         fwrite(&byte, 1, 1, f);
     }
 
+    /* Close destination file and LitePCIe device. */
     fclose(f);
-
     close(fd);
 }
 
@@ -193,24 +245,28 @@ static void flash_reload(void)
 {
     int fd;
 
+    /* Open LitePCIe device. */
     fd = open(litepcie_device, O_RDWR);
     if (fd < 0) {
         fprintf(stderr, "Could not init driver\n");
         exit(1);
     }
 
-    /* reload fpga */
+    /* Reload FPGA through ICAP.*/
     litepcie_reload(fd);
 
+    /* Notice user to reboot the hardware.*/
     printf("================================================================\n");
     printf("= PLEASE REBOOT YOUR HARDWARE TO START WITH NEW FPGA GATEWARE  =\n");
     printf("================================================================\n");
 
+    /* Close LitePCIe device. */
     close(fd);
 }
 #endif
 
-/* dma */
+/* DMA */
+/*-----*/
 
 static inline int64_t add_mod_int(int64_t a, int64_t b, int64_t m)
 {
@@ -221,11 +277,14 @@ static inline int64_t add_mod_int(int64_t a, int64_t b, int64_t m)
 }
 
 #ifdef DMA_CHECK_DATA
+
 static inline uint32_t seed_to_data(uint32_t seed)
 {
 #ifdef DMA_RANDOM_DATA
+    /* Return pseudo random data from seed. */
     return seed * 69069 + 1;
 #else
+    /* Return seed. */
     return seed;
 #endif
 }
@@ -293,7 +352,7 @@ static void dma_test(uint8_t zero_copy, uint8_t external_loopback, int data_widt
         exit(1);
     }
 
-    /* stats */
+    /* Statistics */
     int i = 0;
     int64_t reader_sw_count_last = 0;
     int64_t last_time;
@@ -307,13 +366,16 @@ static void dma_test(uint8_t zero_copy, uint8_t external_loopback, int data_widt
 
     signal(SIGINT, intHandler);
 
+    printf("\e[1m[> DMA loopback test:\e[0m\n");
+    printf("---------------------\n");
+
     if (litepcie_dma_init(&dma, litepcie_device, zero_copy))
         exit(1);
 
-    /* test loop */
+    /* Test loop. */
     last_time = get_time_ms();
     for (;;) {
-        /* exit loop on ctrl+c pressed */
+        /* Exit loop on CTRL+C. */
         if (!keep_running)
             break;
 
@@ -323,38 +385,39 @@ static void dma_test(uint8_t zero_copy, uint8_t external_loopback, int data_widt
         char *buf_wr;
         char *buf_rd;
 
-        /* write tx-buffers */
+        /* Write TX-buffers. */
         while (1) {
-            /* get buffer */
+            /* Get buffer. */
             buf_wr = litepcie_dma_next_write_buffer(&dma);
 
-            /* break when no buffer available */
+            /* Break when no buffer available. */
             if (!buf_wr)
                 break;
 
-            /* write buffer */
+            /* Write buffer. */
             write_pn_data((uint32_t *) buf_wr, DMA_BUFFER_SIZE / sizeof(uint32_t), &seed_wr, data_width);
         }
 
-        /* read/check rx-buffers */
+        /* Read/check RX-buffers */
         while (1) {
-            /* get buffer */
+            /* Get RX-buffer. */
             buf_rd = litepcie_dma_next_read_buffer(&dma);
 
-            /* break when no buffer available */
+            /* Break when no buffer available. */
             if (!buf_rd)
                 break;
 
-            /* skip the first 128 dma loop */
+            /* Skip the first 128 DMA loops. */
             if (dma.writer_hw_count < 128*DMA_BUFFER_COUNT)
                 break;
 
+            /* When running... */
             if (run) {
-                /* check buffer */
+                /* Check buffer. */
                 errors += check_pn_data((uint32_t *) buf_rd, DMA_BUFFER_SIZE / sizeof(uint32_t), &seed_rd, data_width);
                 memset(buf_rd, 0, DMA_BUFFER_SIZE);
             } else {
-                /* find delay/seed */
+                /* Find initial delay/seed. */
                 uint32_t errors_min = 0xffffffff;
                 for (int delay = 0; delay < DMA_BUFFER_SIZE / sizeof(uint32_t); delay++) {
                     seed_rd = delay;
@@ -379,7 +442,7 @@ static void dma_test(uint8_t zero_copy, uint8_t external_loopback, int data_widt
         }
 #endif
 
-        /* statistics */
+        /* Test loop statistics */
         int64_t duration = get_time_ms() - last_time;
         if (run & (duration > 200)) {
             if (i % 10 == 0)
@@ -401,50 +464,8 @@ end:
     litepcie_dma_cleanup(&dma);
 }
 
-
-void scratch_test(void)
-{
-    int fd;
-
-    fd = open(litepcie_device, O_RDWR);
-    if (fd < 0) {
-        fprintf(stderr, "Could not init driver\n");
-        exit(1);
-    }
-
-    printf("Write 0x12345678 to scratch register:\n");
-    litepcie_writel(fd, CSR_CTRL_SCRATCH_ADDR, 0x12345678);
-    printf("Read: 0x%08x\n", litepcie_readl(fd, CSR_CTRL_SCRATCH_ADDR));
-
-    printf("Write 0xdeadbeef to scratch register:\n");
-    litepcie_writel(fd, CSR_CTRL_SCRATCH_ADDR, 0xdeadbeef);
-    printf("Read: 0x%08x\n", litepcie_readl(fd, CSR_CTRL_SCRATCH_ADDR));
-
-    close(fd);
-}
-
-#ifdef CSR_UART_XOVER_RXTX_ADDR
-void uart_test(void)
-{
-    int fd;
-
-    fd = open(litepcie_device, O_RDWR);
-    if (fd < 0) {
-        fprintf(stderr, "Could not init driver\n");
-        exit(1);
-    }
-
-    litepcie_writel(fd, CSR_CTRL_RESET_ADDR, 1); /* reset CPU */
-
-    while (1) {
-        if ((litepcie_readl(fd, CSR_UART_XOVER_RXEMPTY_ADDR) & 0x1) == 0) {
-            printf("%c", litepcie_readl(fd, CSR_UART_XOVER_RXTX_ADDR) & 0xff);
-        }
-    }
-
-    close(fd);
-}
-#endif
+/* HELP */
+/*------*/
 
 static void help(void)
 {
@@ -452,29 +473,30 @@ static void help(void)
            "usage: litepcie_util [options] cmd [args...]\n"
            "\n"
            "options:\n"
-           "-h                                Help\n"
-           "-c device_num                     Select the device (default = 0)\n"
-           "-z                                Enable zero-copy DMA mode\n"
-           "-e                                Use external loopback (default = internal)\n"
-           "-w data_width                     Width of data bus (default = 16)\n"
-           "-a                                Automatic DMA RX-Delay calibration\n"
+           "-h                                Help.\n"
+           "-c device_num                     Select the device (default = 0).\n"
+           "-z                                Enable zero-copy DMA mode.\n"
+           "-e                                Use external loopback (default = internal).\n"
+           "-w data_width                     Width of data bus (default = 16).\n"
+           "-a                                Automatic DMA RX-Delay calibration.\n"
            "\n"
            "available commands:\n"
-           "info                              Board information\n"
-           "dma_test                          Test DMA\n"
-           "scratch_test                      Test Scratch register\n"
-#ifdef CSR_UART_XOVER_RXTX_ADDR
-           "uart_test                         Test CPU Crossover UART\n"
-#endif
+           "info                              Get Board information.\n"
+           "\n"
+           "dma_test                          Test DMA.\n"
+           "scratch_test                      Test Scratch register.\n"
            "\n"
 #ifdef CSR_FLASH_BASE
-           "flash_write filename [offset]     Write file contents to SPI Flash\n"
+           "flash_write filename [offset]     Write file contents to SPI Flash.\n"
            "flash_read filename size [offset] Read from SPI Flash and write contents to file.\n"
            "flash_reload                      Reload FPGA Image.\n"
 #endif
            );
     exit(1);
 }
+
+/* MAIN */
+/*------*/
 
 int main(int argc, char **argv)
 {
@@ -485,14 +507,13 @@ int main(int argc, char **argv)
     static int litepcie_data_width;
     static int litepcie_auto_rx_delay;
 
-
     litepcie_device_num = 0;
     litepcie_data_width = 16;
     litepcie_auto_rx_delay = 0;
     litepcie_device_zero_copy = 0;
     litepcie_device_external_loopback = 0;
 
-    /* parameters */
+    /* Parameters. */
     for (;;) {
         c = getopt(argc, argv, "hc:w:zea");
         if (c == -1)
@@ -524,25 +545,18 @@ int main(int argc, char **argv)
     if (optind >= argc)
         help();
 
-    /* select device */
+    /* Select device */
     snprintf(litepcie_device, sizeof(litepcie_device), "/dev/litepcie%d", litepcie_device_num);
 
     cmd = argv[optind++];
 
+    /* Info cmds */
     if (!strcmp(cmd, "info"))
         info();
-    else if (!strcmp(cmd, "dma_test"))
-        dma_test(
-            litepcie_device_zero_copy,
-            litepcie_device_external_loopback,
-            litepcie_data_width,
-            litepcie_auto_rx_delay);
+    /* Scratch cmds */
     else if (!strcmp(cmd, "scratch_test"))
         scratch_test();
-#ifdef CSR_UART_XOVER_RXTX_ADDR
-    else if (!strcmp(cmd, "uart_test"))
-        uart_test();
-#endif
+    /* SPI Flash cmds */
 #if CSR_FLASH_BASE
     else if (!strcmp(cmd, "flash_write")) {
         const char *filename;
@@ -569,6 +583,15 @@ int main(int argc, char **argv)
     else if (!strcmp(cmd, "flash_reload"))
         flash_reload();
 #endif
+    /* DMA cmds */
+    else if (!strcmp(cmd, "dma_test"))
+        dma_test(
+            litepcie_device_zero_copy,
+            litepcie_device_external_loopback,
+            litepcie_data_width,
+            litepcie_auto_rx_delay);
+
+    /* Help */
     else
         goto show_help;
 
