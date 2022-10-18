a5704232b5538ff22d0cfc1c4826c5f18ecff51a
Florent Kermarrec <florent@enjoy-digital.fr>
2022-10-18 16:37:34 +0200

    frontend/dma/LitePCIeDMAWriter: Revert 0c7b741ce1760b6dbf3bd3f884462206c7f26647.

--- litepcie/frontend/dma.py
+++ litepcie/frontend/dma.py
@@ -481,10 +481,8 @@ class LitePCIeDMAWriter(Module, AutoCSR):
                 data_fifo.source.ready.eq(~splitter.terminate),
                 # When last...
                 If(port.source.last,
-                    # Accept Descriptor (Only when last).
-                    If(splitter.source.last,
-                        splitter.source.ready.eq(1)
-                    ),
+                    # Accept Descriptor.
+                    splitter.source.ready.eq(1),
                     # Accept Data (Force).
                     data_fifo.source.ready.eq(1),
                     # Return to Idle.
