844ead5450dc40197484f94a518ab30568cdbd1b
Florent Kermarrec <florent@enjoy-digital.fr>
2023-04-03 17:50:38 +0200

    phy/xilinx_us_gen3_x8: Similar adaptation than x4.

--- litepcie/phy/uspciephy.py
+++ litepcie/phy/uspciephy.py
@@ -376,10 +376,17 @@ class USPCIEPHY(LiteXModule):
     def add_sources(self, platform, phy_path, phy_filename):
         platform.add_ip(os.path.join(phy_path, phy_filename))
         platform.add_source(os.path.join(phy_path, "..", "axis_iff.v"))
+
         platform.add_source(os.path.join(phy_path, "..", "s_axis_rq_adapt_x4.v"))
         platform.add_source(os.path.join(phy_path, "..", "m_axis_rc_adapt_x4.v"))
         platform.add_source(os.path.join(phy_path, "..", "m_axis_cq_adapt_x4.v"))
         platform.add_source(os.path.join(phy_path, "..", "s_axis_cc_adapt_x4.v"))
+
+        platform.add_source(os.path.join(phy_path, "..", "s_axis_rq_adapt_x8.v"))
+        platform.add_source(os.path.join(phy_path, "..", "m_axis_rc_adapt_x8.v"))
+        platform.add_source(os.path.join(phy_path, "..", "m_axis_cq_adapt_x8.v"))
+        platform.add_source(os.path.join(phy_path, "..", "s_axis_cc_adapt_x8.v"))
+
         platform.add_source(os.path.join(phy_path, "pcie_us_support.v"))
 
     # External Hard IP -----------------------------------------------------------------------------
