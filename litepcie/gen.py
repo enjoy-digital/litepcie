51539fd0627f8c7cf5c008a039a31e671f250069
Florent Kermarrec <florent@enjoy-digital.fr>
2023-08-01 14:49:21 +0200

    gen: Remove useless WaitTimer import.

--- litepcie/gen.py
+++ litepcie/gen.py
@@ -34,7 +34,6 @@ from migen import *
 from migen.genlib.resetsync import AsyncResetSynchronizer
 
 from litex.gen import *
-from litex.gen.genlib.misc import WaitTimer
 
 from litex.soc.cores.clock import *
 from litex.soc.interconnect.csr import *
