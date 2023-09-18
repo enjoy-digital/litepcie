2094abe11ce8a8dab1240214717143de429f42b5
Florent Kermarrec <florent@enjoy-digital.fr>
2023-09-18 08:43:26 +0200

    setup.py: Update to 2023.08.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fp:
 
 setup(
     name="litepcie",
-    version="2023.04",
+    version="2023.08",
     description="Small footprint and configurable PCIe core",
     long_description=long_description,
     long_description_content_type="text/markdown",
