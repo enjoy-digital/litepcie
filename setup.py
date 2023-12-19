eeaf7e0b9b043ed8dd839bd2fc2aa5c2f4d8d632
Florent Kermarrec <florent@enjoy-digital.fr>
2023-12-19 09:21:43 +0100

    setup.py: Improve indentation.

--- setup.py
+++ setup.py
@@ -9,20 +9,20 @@ with open("README.md", "r") as fp:
 
 
 setup(
-    name="litepcie",
-    version="2023.08",
-    description="Small footprint and configurable PCIe core",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Florent Kermarrec",
-    author_email="florent@enjoy-digital.fr",
-    url="http://enjoy-digital.fr",
-    download_url="https://github.com/enjoy-digital/litepcie",
-    test_suite="test",
-    license="BSD",
-    python_requires="~=3.6",
-    install_requires=["pyyaml", "litex"],
-    extras_require={
+    name                          = "litepcie",
+    version                       = "2023.08",
+    description                   = "Small footprint and configurable PCIe core",
+    long_description              = long_description,
+    long_description_content_type = "text/markdown",
+    author                        = "Florent Kermarrec",
+    author_email                  = "florent@enjoy-digital.fr",
+    url                           = "http://enjoy-digital.fr",
+    download_url                  = "https://github.com/enjoy-digital/litepcie",
+    test_suite                    = "test",
+    license                       = "BSD",
+    python_requires               = "~=3.6",
+    install_requires              = ["pyyaml", "litex"],
+    extras_require                = {
         "develop": [
           "meson"
           "pexpect"
@@ -30,19 +30,19 @@ setup(
           "requests"
         ]
     },
-    packages=find_packages(exclude=("test*", "sim*", "doc*", "examples*")),
-    include_package_data=True,
-    keywords="HDL ASIC FPGA hardware design",
-    classifiers=[
-        "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
-        "Environment :: Console",
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
+    packages                      = find_packages(exclude=("test*", "sim*", "doc*", "examples*")),
+    include_package_data          = True,
+    keywords                      = "HDL ASIC FPGA hardware design",
+    classifiers                   = [
+        "Topic                :: Scientific/Engineering :: Electronic Design Automation (EDA)",
+        "Environment          :: Console",
+        "Development Status   :: 3 - Alpha",
+        "Intended Audience    :: Developers",
+        "License              :: OSI Approved :: BSD License",
+        "Operating System     :: OS Independent",
         "Programming Language :: Python",
     ],
-    entry_points={
+    entry_points = {
         "console_scripts": [
             "litepcie_gen=litepcie.gen:main",
         ],
