c2f8b42eabba79f051f681d97870604f02166df9
Florent Kermarrec <florent@enjoy-digital.fr>
2024-02-11 14:23:13 +0100

    test/test_wishbone: Update/Simplify.

--- test/test_wishbone.py
+++ test/test_wishbone.py
@@ -1,8 +1,27 @@
 #
 # This file is part of LitePCIe.
 #
-# Copyright (c) 2015-2022 Florent Kermarrec <florent@enjoy-digital.fr>
+# Copyright (c) 2015-2024 Florent Kermarrec <florent@enjoy-digital.fr>
 # SPDX-License-Identifier: BSD-2-Clause
+
+import unittest
+
+from litex.gen import *
+
+from litex.soc.interconnect import wishbone
+
+from litepcie.core import LitePCIeEndpoint
+from litepcie.frontend.wishbone import LitePCIeWishboneMaster
+
+from test.common import seed_to_data
+from test.model.host import *
+
+# Parameters ---------------------------------------------------------------------------------------
+
+root_id     = 0x100
+endpoint_id = 0x400
+
+# Test Wishbone Master -----------------------------------------------------------------------------
 
 # In this high level test, LitePCIeEndpoint is connected to LitePCIeWishboneBridge frontend, itself
 # connected to a Wishbone SRAM and our Host software model is used to generate Write/Read TLPs:
@@ -38,26 +57,7 @@
 # The test verifies that the Host model is able to access the wishbone SRAM correctly through the
 # LitePCIeEndpoint.
 
-import unittest
-
-from migen import *
-
-from litex.soc.interconnect import wishbone
-
-from litepcie.core import LitePCIeEndpoint
-from litepcie.frontend.wishbone import LitePCIeWishboneBridge
-
-from test.common import seed_to_data
-from test.model.host import *
-
-# Parameters ---------------------------------------------------------------------------------------
-
-root_id     = 0x100
-endpoint_id = 0x400
-
-# Test Wishbone ------------------------------------------------------------------------------------
-
-class TestWishbone(unittest.TestCase):
+class TestWishboneMaster(unittest.TestCase):
     def wishbone_test(self, data_width, nwords=64):
         wr_datas = [seed_to_data(i, True) for i in range(nwords)]
         rd_datas = []
@@ -71,12 +71,12 @@ class TestWishbone(unittest.TestCase):
                 yield from dut.host.chipset.rd32(i)
                 rd_datas.append(dut.host.chipset.rd_data[0])
 
-        class DUT(Module):
+        class DUT(LiteXModule):
             def __init__(self, data_width):
-                self.submodules.host            = Host(data_width, root_id, endpoint_id)
-                self.submodules.endpoint        = LitePCIeEndpoint(self.host.phy)
-                self.submodules.wishbone_bridge = LitePCIeWishboneBridge(self.endpoint)
-                self.submodules.sram            = wishbone.SRAM(nwords*4, bus=self.wishbone_bridge.wishbone)
+                self.host     = Host(data_width, root_id, endpoint_id)
+                self.endpoint = LitePCIeEndpoint(self.host.phy)
+                self.master   = LitePCIeWishboneMaster(self.endpoint)
+                self.sram     = wishbone.SRAM(nwords*4, bus=self.master.wishbone)
 
         dut = DUT(data_width)
         generators = {
