1218cb8f117fcbe0ade82ae3e6794de71461bb82
Florent Kermarrec <florent@enjoy-digital.fr>
2024-07-22 11:56:32 +0200

    software/kernel: Cleanup flags (from LiteXM2SDR).

--- litepcie/software/kernel/flags.h
+++ litepcie/software/kernel/flags.h
@@ -12,12 +12,12 @@
 #define __HW_FLAGS_H
 
 /* spi */
-#define SPI_CTRL_START 0x1
-#define SPI_CTRL_LENGTH (1<<8)
-#define SPI_STATUS_DONE 0x1
+#define SPI_CTRL_START  (1 << 0)
+#define SPI_CTRL_LENGTH (1 << 8)
+#define SPI_STATUS_DONE (1 << 0)
 
 /* pcie */
-#define DMA_TABLE_LOOP_INDEX (1 << 0)
+#define DMA_TABLE_LOOP_INDEX (1 <<  0)
 #define DMA_TABLE_LOOP_COUNT (1 << 16)
 
 #endif /* __HW_FLAGS_H */
