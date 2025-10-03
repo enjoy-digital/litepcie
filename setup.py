2b6980c0735b81619365257c644baeac6377e6f3
Florent Kermarrec <florent@enjoy-digital.fr>
2025-10-03 10:37:40 +0200

    Bump to version 2025.08

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fp:
 
 setup(
     name                          = "litepcie",
-    version                       = "2025.04",
+    version = "2025.08",
     description                   = "Small footprint and configurable PCIe core",
     long_description              = long_description,
     long_description_content_type = "text/markdown",
